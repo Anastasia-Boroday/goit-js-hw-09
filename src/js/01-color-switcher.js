0afc6bc1fd2e834e21f996c367763f8a59361c67
Anastasia-Boroday <anastasiaborodaj22@gmail.com>
2022-11-23 23:00:43 +0200

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-console.log('hi');
+const refs = {
+    start: document.querySelector("button[data-start]"),
+    stop: document.querySelector("button[data-stop]"),
+    body:document.querySelector("body"),
+}
+let timerId = 0;
+const TIME = 1000;
+
+refs.start.addEventListener("click", startTimer);
+refs.stop.addEventListener("click", stopTimer);
+
+function startTimer() {
+    refs.start.setAttribute('disabled', true);
+    refs.stop.removeAttribute('disabled');
+    intervalId = setInterval(() => {
+        refs.body.style.background = getRandomHexColor();
+    }, TIME);
+}
+
+function stopTimer() {
+    refs.start.removeAttribute('disabled');
+    refs.stop.setAttribute('disabled', true);
+    clearInterval(intervalId);   
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
