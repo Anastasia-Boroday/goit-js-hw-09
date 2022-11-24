06053498fdfc887575668c7ab90e374e211edd55
Anastasia-Boroday <93268396+Anastasia-Boroday@users.noreply.github.com>
2022-11-24 22:59:52 +0000

    Deploying to gh-pages from @ Anastasia-Boroday/goit-js-hw-09@4f202f9e5e1c011b60bff6bd5d939ac57a7be4b0 🚀

--- 01-color-switcher.8163c25a.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=0;t.start.addEventListener("click",(function(){t.start.setAttribute("disabled",!0),t.stop.removeAttribute("disabled"),e=setInterval((()=>{t.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){t.start.removeAttribute("disabled"),t.stop.setAttribute("disabled",!0),clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.8163c25a.js.map
